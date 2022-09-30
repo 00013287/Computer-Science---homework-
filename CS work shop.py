3c3e0d270763058c9aa5b6fb41783c3dda411e7c
00013287 <113858602+00013287@users.noreply.github.com>
2022-09-30 22:05:24 +0500

    Delete CS work shop.py
    none

--- CS work shop.py
+++
@@ -1,5 +0,0 @@
-
-
-decimal_number = int(input("Enter a decimal number :"))
-octal_number = oct(decimal_number).replace("0o", "")
-print("The octal value for {} is {}".format(decimal_number,octal_number))
